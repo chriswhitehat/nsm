a2dccc3cfb285bbebbe95037515ed6f06fbd99d9
Chris White <q518500@kp-h4tfv1kcpn7c.local>
2022-02-14 12:35:11 -0800

    b

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures nsm'
 long_description 'Installs/Configures nsm'
 chef_version '>= 14.0'
 
-version '0.2.12'
+version '0.2.13'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
