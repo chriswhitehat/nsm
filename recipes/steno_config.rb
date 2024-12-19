842dcc26994b59883bdab795a9f1f43ccdac9cf6
Chris White <chris.m.white@kp.org>
2024-12-19 11:15:59 -0800

    steno cron fix and iface nil fix

--- recipes/steno_config.rb
+++ recipes/steno_config.rb
@@ -162,6 +162,12 @@ if node[:nsm][:interfaces][:sniffing]
         action :nothing
       end
 
+      cron_d 'steno_#{sniff[:sensorname]}_chown_cron' do
+        user 'stenoghrapher'
+        minute '*'
+        command "find /nsm/steno/#{sniff[:sensorname]}/ -group stenographer | xargs -r chown stenographer:nsm"
+      end
+
       local_port += 1
       grpc_port += 1
 
